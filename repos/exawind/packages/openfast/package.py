33618a2ac13ed490e45baab9a7ec843b6bc18f93
Jon Rood <jon.rood@nrel.gov>
2024-05-03 15:27:36 -0600

    Update openfast.

--- repos/exawind/packages/openfast/package.py
+++ repos/exawind/packages/openfast/package.py
@@ -8,14 +8,4 @@
 from spack.pkg.builtin.openfast import Openfast as bOpenfast
 
 class Openfast(bOpenfast):
-    patch("hub_seg_fault.patch", when="@2.7:3.2")
-    patch("segfault_message.patch", when="%clang@12.0.1 build_type=RelWithDebInfo")
     patch("openmp.patch", when="@develop")
-    version("3.5.3", tag="v3.5.3", commit="6a7a543790f3cad4a65b87242a619ac5b34b4c0f")
-
-    variant("rosco", default=False,
-            description="Build ROSCO controller alongside OpenFAST")
-
-    depends_on("rosco", when="+rosco")
-    depends_on("netcdf-c", when="@develop")
-    depends_on("yaml-cpp@0.6.0:0.6.3", when="+cxx")
