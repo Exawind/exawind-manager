6608e2d5a0e4e2e7bafe0d7515ab398d4b2098a7
Jon Rood <jon.rood@nrel.gov>
2024-05-13 10:33:27 -0600

    Simplify custom packages due to upstreaming to Spack (#48)
    * Simplify nalu-wind and remove custom hypre due to upstreaming to Spack.
    
    * Update Spack.

--- repos/exawind/packages/nalu-wind/package.py
+++ repos/exawind/packages/nalu-wind/package.py
@@ -23,53 +23,17 @@ class NaluWind(CtestPackage, bNaluWind, ROCmPackage):
             description="Turn on address sanitizer")
     variant("fsi", default=False,
             description="Use FSI branch of openfast")
-    variant("shared", default=True,
-            description="Build shared libraries")
-    variant("umpire", default=False,
-            description="Enable Umpire")
-    variant("gpu-aware-mpi", default=False,
-            description="gpu-aware-mpi")
     variant("tests", default=True, description="Activate regression tests")
     variant("unit-tests", default=True, description="Activate unit tests")
 
-    depends_on("hypre+gpu-aware-mpi", when="+gpu-aware-mpi")
-    depends_on("hypre+umpire", when="+umpire")
-    depends_on("trilinos gotype=long cxxstd=17")
-    depends_on("trilinos~shared", when="+trilinos-solvers")
-    depends_on("trilinos+wrapper", when="+cuda")
     depends_on("openfast@develop+netcdf+cxx", when="+fsi")
-
-    for _arch in ROCmPackage.amdgpu_targets:
-        depends_on("trilinos@13.4.0.2022.10.27: ~shared+exodus+tpetra+zoltan+stk~superlu-dist~superlu+hdf5+shards~hypre+gtest+rocm amdgpu_target={0}".format(_arch),
-                   when="+rocm amdgpu_target={0}".format(_arch))
-        depends_on("hypre+rocm amdgpu_target={0}".format(_arch), when="+hypre+rocm amdgpu_target={0}".format(_arch))
-
-    conflicts("+cuda", when="+rocm")
-    conflicts("^hypre+cuda", when="~cuda")
-    conflicts("^hypre+rocm", when="~rocm")
-    conflicts("^hypre+sycl")
-    conflicts("^trilinos+cuda", when="~cuda")
-    conflicts("^trilinos+rocm", when="~rocm")
-    conflicts("+shared", when="+cuda",
-             msg="invalid device functions are generated with shared libs and cuda")
-    conflicts("+shared", when="+rocm",
-             msg="invalid device functions are generated with shared libs and rocm")
 
     def setup_build_environment(self, env):
         super().setup_build_environment(env)
-        env.append_flags("CXXFLAGS", "-DUSE_STK_SIMD_NONE")
         if "+asan" in self.spec:
             env.append_flags("CXXFLAGS", "-fsanitize=address -fno-omit-frame-pointer -fsanitize-blacklist={0}".format(join_path(self.package_dir, "blacklist.asan")))
             env.set("LSAN_OPTIONS", "suppressions={0}".format(join_path(self.package_dir, "sup.asan")))
             env.set("ASAN_OPTIONS", "detect_container_overflow=0")
-        if "+cuda" in self.spec:
-            env.set("CUDA_LAUNCH_BLOCKING", "1")
-            env.set("CUDA_MANAGED_FORCE_DEVICE_ALLOC", "1")
-            env.set("OMPI_CXX", self.spec["kokkos-nvcc-wrapper"].kokkos_cxx)
-            env.set("MPICH_CXX", self.spec["kokkos-nvcc-wrapper"].kokkos_cxx)
-            env.set("MPICXX_CXX", self.spec["kokkos-nvcc-wrapper"].kokkos_cxx)
-        if "+rocm" in self.spec:
-            env.append_flags('CXXFLAGS', '-fgpu-rdc')
 
     def cmake_args(self):
         spec = self.spec
@@ -82,16 +46,6 @@ class NaluWind(CtestPackage, bNaluWind, ROCmPackage):
         if spec.satisfies("dev_path=*"):
             cmake_options.append(self.define("CMAKE_EXPORT_COMPILE_COMMANDS",True))
             cmake_options.append(self.define("ENABLE_TESTS", True))
-
-        if "+umpire" in spec:
-            cmake_options.append(self.define_from_variant("ENABLE_UMPIRE", "umpire"))
-            cmake_options.append(self.define("UMPIRE_DIR", spec["umpire"].prefix))
-
-        if "+rocm" in spec:
-            cmake_options.append(self.define("CMAKE_CXX_COMPILER", spec["hip"].hipcc))
-            cmake_options.append(self.define("ENABLE_ROCM", True))
-            targets = spec.variants["amdgpu_target"].value
-            cmake_options.append(self.define("GPU_TARGETS", ";".join(str(x) for x in targets)))
 
         cmake_options.append(self.define_from_variant("ENABLE_OPENFAST_FSI", "fsi"))
         if "+fsi" in spec:
