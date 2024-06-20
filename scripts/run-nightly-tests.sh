7db681499970cfb7b5d285fe95633d10a708751b
psakiev <psakiev@sandia.gov>
2024-06-20 15:48:37 -0600

    Update commands for working on mac os

--- scripts/run-nightly-tests.sh
+++ scripts/run-nightly-tests.sh
@@ -23,7 +23,9 @@ prune_envs() {
   today=$(date -I)
 
   for env in $(date_spack_envs); do
-    date_diff=$(( ($(date -d "$today" +%s) - $(date -d "$env" +%s)) / 86400 ))
+
+    date_diff=$(( ($(date -j -f "%Y-%m-%d" "$today" +%s) - $(date -j -f "%Y-%m-%d" "$env" +%s)) / 86400 ))
+
     if [ "${date_diff}" -gt "${days_to_keep}" ]; then
      echo "Spack env ${env} is older than ${days_to_keep} days"
      cmd "spack -e ${env} uninstall --all --dependents --force -y"
@@ -34,6 +36,6 @@ prune_envs() {
 
 cmd "prune_envs"
 
-packages_to_test = "exawind amr-wind nalu-wind"
+packages_to_test="exawind amr-wind nalu-wind"
 # build the test environment and run the tests
 cmd "${EXAWIND_MANAGER}/scripts/deploy.py --daily --cdash ${packages_to_test} --ranks ${nranks} --overwrite --regression_tests ${packages_to_test}"
