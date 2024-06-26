817ff81c1066885a5abafbb9e7e289340c0ef7d0
Jon Rood <jon.rood@nrel.gov>
2024-06-26 11:38:59 -0600

    Use depfile in test script. (#86)

--- scripts/run-nightly-tests.sh
+++ scripts/run-nightly-tests.sh
@@ -38,4 +38,4 @@ cmd "prune_envs"
 
 packages_to_test="exawind amr-wind nalu-wind"
 # build the test environment and run the tests
-cmd "${EXAWIND_MANAGER}/scripts/deploy.py --daily --cdash ${packages_to_test} --ranks ${nranks} --overwrite --regression_tests ${packages_to_test}"
+cmd "${EXAWIND_MANAGER}/scripts/deploy.py --depfile --daily --cdash ${packages_to_test} --ranks ${nranks} --overwrite --regression_tests ${packages_to_test}"
