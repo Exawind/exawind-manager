4a6473cc4f5990e2ed8d4f864b3c9df5b337fb3b
Jon Rood <jon.rood@nrel.gov>
2024-07-02 15:24:02 -0600

    Fix something.

--- repos/exawind/packages/pelec/package.py
+++ repos/exawind/packages/pelec/package.py
@@ -4,6 +4,7 @@
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
 from spack.package import *
+from spack.pkg.exawind.ctest_package import *
 
 
 class Pelec(CMakePackage, CudaPackage, ROCmPackage, CtestPackage):
