7da6150813cd4a91f28573230282095c915f7429
Jon Rood <jon.rood@nrel.gov>
2024-07-02 14:16:34 -0600

    Add more testing stuff to Pele.

--- repos/exawind/packages/pelec/package.py
+++ repos/exawind/packages/pelec/package.py
@@ -6,7 +6,7 @@
 from spack.package import *
 
 
-class Pelec(CMakePackage, CudaPackage, ROCmPackage):
+class Pelec(CMakePackage, CudaPackage, ROCmPackage, CtestPackage):
     """An AMR code for compressible reacting flow simulations."""
 
     homepage = "https://github.com/AMReX-Combustion/PeleC"
@@ -20,6 +20,8 @@ class Pelec(CMakePackage, CudaPackage, ROCmPackage):
 
     version("main", branch="development", submodules=True)
 
+    variant("asan", default=False, description="Turn on address sanitizer")
+    variant("clangtidy", default=False, description="Turn on clang-tidy")
     variant(
         "dim",
         default="3",
@@ -40,6 +42,7 @@ class Pelec(CMakePackage, CudaPackage, ROCmPackage):
     variant("openmp", default=False, description="Enable OpenMP for CPU builds")
     variant("particles", default=False, description="Enable AMReX particles")
     variant("shared", default=True, description="Build shared libraries")
+    variant("tests", default=False, description="Enable some things for testing")
     variant("tiny_profile", default=True, description="Activate tiny profile")
     variant("hdf5", default=False, description="Enable HDF5 plots with ZFP compression")
     variant("sycl", default=False, description="Enable SYCL backend")
@@ -62,6 +65,15 @@ class Pelec(CMakePackage, CudaPackage, ROCmPackage):
     conflicts("+openmp", when="+rocm")
     conflicts("+openmp", when="+sycl")
 
+    requires("+tests", when="+cdash_submit")
+
+    def setup_build_environment(self, env):
+        spec = self.spec
+        super().setup_build_environment(env)
+        if spec.satisfies("+asan"):
+            env.append_flags("CXXFLAGS", "-fsanitize=address -fno-omit-frame-pointer")
+            env.set("LSAN_OPTIONS", "suppressions={0}".format(join_path(self.package_dir, "sup.asan")))
+
     def cmake_args(self):
         define = self.define
         spec = self.spec
@@ -83,6 +95,7 @@ class Pelec(CMakePackage, CudaPackage, ROCmPackage):
             self.define_from_variant("BUILD_SHARED_LIBS", "shared"),
             self.define_from_variant("PELE_DIM", "dim"),
             self.define_from_variant("PELE_PRECISION", "precision"),
+            self.define_from_variant("PELE_ENABLE_CLANG_TIDY", "clangtidy"))
         ]
 
         if spec.satisfies("+mpi"):
@@ -117,5 +130,14 @@ class Pelec(CMakePackage, CudaPackage, ROCmPackage):
                     "or the oneAPI CXX (icpx) compiler."
                 ),
             )
+
+        if spec.satisfies("dev_path=*"):
+            cmake_options.append(self.define("CMAKE_EXPORT_COMPILE_COMMANDS", True))
+
+        if spec.satisfies("+tests"):
+            cmake_options.append(self.define("PELE_ENABLE_FCOMPARE_FOR_TESTS", True))
+            cmake_options.append(self.define("PELE_SAVE_GOLDS", True))
+            cmake_options.append(self.define("PELE_SAVED_GOLDS_DIRECTORY", self.saved_golds_dir))
+            cmake_options.append(self.define("PELE_REFERENCE_GOLDS_DIRECTORY", self.reference_golds_dir))
 
         return args
