969de98b87d0944f2f9beac6474c2a2476a1af98
Jon Rood <jon.rood@nrel.gov>
2024-09-23 14:46:05 -0600

    Fix mistake.

--- scripts/run-nightly-tests.sh
+++ scripts/run-nightly-tests.sh
@@ -33,5 +33,5 @@ prune_envs() {
 
 cmd "prune_envs"
 
-packages_to_test="amr-wind"
+packages_to_test="exawind amr-wind nalu-wind"
 cmd "${EXAWIND_MANAGER}/scripts/deploy.py --depfile --daily --cdash ${packages_to_test} --ranks ${nranks} --overwrite --regression_tests ${packages_to_test}"
