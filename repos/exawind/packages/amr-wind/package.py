1335e631b3a6d8f9bf6ff061e4c04fb6aed87396
Jon Rood <jon.rood@nrel.gov>
2024-10-28 19:59:34 -0600

    Remove amr-wind@main preferred version.

--- repos/exawind/packages/amr-wind/package.py
+++ repos/exawind/packages/amr-wind/package.py
@@ -10,7 +10,7 @@ from spack.pkg.builtin.amr_wind import AmrWind as bAmrWind
 from spack.pkg.exawind.ctest_package import *
 
 class AmrWind(bAmrWind, CtestPackage):
-    version("main", branch="main", submodules=True, preferred=True)
+    version("main", branch="main", submodules=True)
     version("multiphase", branch="multiphase_dev", submodules=True)
     
     variant("asan", default=False, description="Turn on address sanitizer")
