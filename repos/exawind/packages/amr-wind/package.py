50e3c1753748d617b2ddcb72a4096b3d94870b0f
Jon Rood <jon.rood@nrel.gov>
2024-12-31 11:13:35 -0700

    Add py-netcdf4 to amr-wind dependencies.

--- repos/exawind/packages/amr-wind/package.py
+++ repos/exawind/packages/amr-wind/package.py
@@ -15,6 +15,8 @@ class AmrWind(bAmrWind, CtestPackage):
     variant("asan", default=False, description="Turn on address sanitizer")
     variant("clangtidy", default=False, description="Turn on clang-tidy")
 
+    depends_on("py-netcdf4", when="+netcdf")
+
     requires("+tests", when="+cdash_submit")
 
     def setup_build_environment(self, env):
