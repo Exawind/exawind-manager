9fb70587e31dfb1577534481c66d00de83e822ed
Jon Rood <jon.rood@nrel.gov>
2025-03-21 22:15:16 -0600

    Remove uneeded patch.

--- repos/exawind/packages/trilinos/package.py
+++ repos/exawind/packages/trilinos/package.py
@@ -12,8 +12,6 @@ from spack.pkg.builtin.trilinos import Trilinos as bTrilinos
 class Trilinos(bTrilinos):
     variant("asan", default=False, description="Turn on address sanitizer")
 
-    #patch("stk_mesh_ngpfield_template.patch", when="@develop")
-
     def setup_build_environment(self, env):
         spec = self.spec
         super().setup_build_environment(env)
