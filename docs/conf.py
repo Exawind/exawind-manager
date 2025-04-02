5d50a6291ed78822b3aad6b64acdfb604cab2297
Jon Rood <jon.rood@nrel.gov>
2025-04-02 10:54:29 -0600

    More documentation updates and update Azure configuration (#271)

--- docs/conf.py
+++ docs/conf.py
@@ -2,9 +2,9 @@ extensions = ["myst_parser"]
 myst_heading_anchors = 3
 templates_path = ["_templates"]
 master_doc = "index"
-project = "Spack-Manager"
-copyright = "Phil Sakievich"
-author = "Phil Sakievich"
+project = "Exawind-Manager"
+copyright = "Phil Sakievich and Jon Rood"
+author = "Phil Sakievich and Jon Rood"
 version = "0.1"
 release = "0.1"
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
@@ -17,19 +17,19 @@ html_static_path = []
 html_theme_options = {"navigation_depth": 5}
 html_show_sourcelink = True
 html_show_copyright = False
-htmlhelp_basename = "spack-manager-doc"
+htmlhelp_basename = "exawind-manager-doc"
 latex_elements = {}
 latex_documents = [
-    (master_doc, "spack-manager.tex", "Spack-Manager Documentation", author, "manual")
+    (master_doc, "exawind-manager.tex", "Exawind-Manager Documentation", author, "manual")
 ]
-man_pages = [(master_doc, "spack-manager", "Spack-Manager Documentation", [author], 1)]
+man_pages = [(master_doc, "exawind-manager", "Exawind-Manager Documentation", [author], 1)]
 texinfo_documents = [
     (
         master_doc,
-        "spack-manager",
-        "Spack-Manager Documentation",
+        "exawind-manager",
+        "Exawind-Manager Documentation",
         author,
-        "Spack-Manager",
+        "Exawind-Manager",
         "One line description of project.",
         "Miscellaneous",
     )
