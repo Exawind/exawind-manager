1add4100fa5dd54f2ad4aca2fe32f129cfe7d306
Jon Rood <jon.rood@nrel.gov>
2025-05-08 19:45:46 -0600

    Remove stuff.

--- repos/exawind/packages/netcdf_c/package.py
+++ repos/exawind/packages/netcdf_c/package.py
@@ -1,6 +1,4 @@
-from spack.package import *
 from spack_repo.builtin.packages.netcdf_c.package import NetcdfC as bNetcdfC
-from spack.pkg.exawind.ctest_package import *
 
 class NetcdfC(bNetcdfC):
     depends_on("cxx", type="build")
