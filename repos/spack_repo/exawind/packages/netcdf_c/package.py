0144b2f2c1db23110450cb19c1487b27ed678e8d
Jon Rood <jon.rood@nrel.gov>
2025-05-15 21:18:02 -0600

    Try adding cmake args to netcdf.

--- repos/spack_repo/exawind/packages/netcdf_c/package.py
+++ repos/spack_repo/exawind/packages/netcdf_c/package.py
@@ -4,3 +4,9 @@ from spack_repo.builtin.packages.netcdf_c.package import NetcdfC as bNetcdfC
 class NetcdfC(bNetcdfC):
     depends_on("c", type="build")
     depends_on("cxx", type="build")
+
+    def cmake_args(self):
+        spec = self.spec
+        cmake_options = super(NetcdfC, self).cmake_args()
+        
+        return cmake_options
