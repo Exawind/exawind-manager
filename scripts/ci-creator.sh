4e212b36869833ca5eba3ed46d0e1450ac1a722e
Jon Rood <jon.rood@nrel.gov>
2025-06-02 14:59:35 -0600

    Try some tweaks to CI creator. (#311)

--- scripts/ci-creator.sh
+++ scripts/ci-creator.sh
@@ -2,5 +2,5 @@
 
 source ${EXAWIND_MANAGER}/start.sh
 spack-start
-deploy.py --name e4s-build --ranks=${NRANKS:-4} --depfile
 spack clean -a
+python3 deploy.py --name e4s-build --ranks=${NRANKS:-4} --depfile
