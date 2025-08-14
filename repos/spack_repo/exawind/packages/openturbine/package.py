28f92f0b6a3a30f8c611c1c8673f5a1d089def19
Jon Rood <jon.rood@nrel.gov>
2025-08-14 10:09:40 -0600

    Add openturbine to testing.

---
+++ repos/spack_repo/exawind/packages/openturbine/package.py
@@ -0,0 +1,25 @@
+from spack.package import *
+from spack_repo.builtin.packages.amr_wind.package import Openturbine as bOpenturbine
+from spack_repo.exawind.packages.ctest_package.package import *
+
+
+class Openturbine(bOpenturbine, CtestPackage):
+    variant("asan", default=False, description="Turn on address sanitizer")
+
+    requires("+tests", when="+cdash_submit")
+
+    def setup_build_environment(self, env):
+        spec = self.spec
+        super().setup_build_environment(env)
+        if spec.satisfies("+asan"):
+            env.append_flags("CXXFLAGS", "-fsanitize=address -fno-omit-frame-pointer")
+            env.set("LSAN_OPTIONS", "suppressions={0}".format(join_path(self.package_dir, "sup.asan")))
+
+    def cmake_args(self):
+        spec = self.spec
+        cmake_options = super(Openturbine, self).cmake_args()
+
+        if spec.satisfies("dev_path=*"):
+            cmake_options.append(self.define("CMAKE_EXPORT_COMPILE_COMMANDS", True))
+
+        return cmake_options
